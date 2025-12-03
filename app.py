08f79decf45bb878c4f1250e2bf73db93a3daaad
Shahd Khalil <114525646+ShahdKh5@users.noreply.github.com>
2025-12-03 10:40:51 +0200

    Change home route to render index.html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from extensions import db  # <-- import db from extensions
 from sqlalchemy import text
+from flask import Flask, render_template
 
 app = Flask(__name__)
 CORS(app)
@@ -46,13 +47,14 @@ app.register_blueprint(dashboard_bp)
 app.register_blueprint(friend_bp)
 
 @app.route("/")
-def home():
-    return jsonify({"message": "Studify API Running"})
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     # with app.app_context():
     #     db.create_all()
     app.run(debug=True)
+
 
 
 
