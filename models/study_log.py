9da25800c0a24243d990c0f0c4c92e996464e5af
Marev-Wasim <marevwasim79@gmail.com>
2025-12-10 01:30:47 +0200

    Update and rename activity_log.py to study_log.py

---
+++ models/study_log.py
@@ -0,0 +1,13 @@
+from extensions import db
+from datetime import datetime
+
+class StudyLog(db.Model):
+    __tablename__ = 'study_logs'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
+    study_date = db.Column(db.Date, nullable=False)
+    hours_studied = db.Column(db.Numeric(4, 2), nullable=False)
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.task_id'), nullable=True)
+    # date = db.Column(db.DateTime, default=datetime.utcnow)
+    #hours = db.Column(db.Float, nullable=False)
