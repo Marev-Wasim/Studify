d1d8122d64909c1509867d1c111b89c0a7b0d43a
Shahd Khalil <114525646+ShahdKh5@users.noreply.github.com>
2025-12-13 15:09:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ from sqlalchemy import text
 
 app = Flask(__name__)
 CORS(app)
-#CORS(
-    #app, 
-    #resources={r"/*": {"origins": ["http://192.168.52.1:3000"]}},
-    #supports_credentials=True,
-    #allow_headers=["Content-Type", "Authorization"] # Explicitly allow common headers
-#)
+
 app.config.from_object('config.Config')
 
 # Initialize db with app
@@ -62,6 +57,7 @@ if __name__ == "__main__":
     # with app.app_context():
     #     db.create_all()
     app.run(debug=True)
+
 
 
 
