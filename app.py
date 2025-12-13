85adc89908adeca006170e77647907a27a9955f5
Shahd Khalil <114525646+ShahdKh5@users.noreply.github.com>
2025-12-13 15:12:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from sqlalchemy import text
 
 app = Flask(__name__)
 CORS(app)
-
 app.config.from_object('config.Config')
 
 # Initialize db with app
@@ -57,6 +56,7 @@ if __name__ == "__main__":
     # with app.app_context():
     #     db.create_all()
     app.run(debug=True)
+
 
 
 
