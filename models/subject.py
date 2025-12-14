82fc002a7d946186594ce63ddc3bd2b2a0e947f0
Marev-Wasim <marevwasim79@gmail.com>
2025-12-14 23:11:38 +0200

    Update subject.py

--- models/subject.py
+++ models/subject.py
@@ -3,8 +3,9 @@ from extensions import db
 class Subject(db.Model):
     __tablename__ = 'subjects'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    color = db.Column(db.String(20))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    #color = db.Column(db.String(20))
 
     tasks = db.relationship('Task', backref='subject', lazy=True)
+    
