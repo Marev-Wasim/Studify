99272ee66635d65792f8c99e4919ecc2722af837
Marev-Wasim <marevwasim79@gmail.com>
2025-12-16 17:52:39 +0200

    Update updates.sql

--- db/updates.sql
+++ db/updates.sql
@@ -6,5 +6,11 @@ ADD task_id INT NULL;
 
 ALTER TABLE study_logs 
 ADD CONSTRAINT study_logs_task 
-    FOREIGN KEY (task_id) REFERENCES tasks(task_id);
+FOREIGN KEY (task_id) REFERENCES tasks(task_id);
 
+ALTER TABLE friends 
+ADD sent_by_id INT NOT NULL;
+
+ALTER TABLE friends 
+ADD CONSTRAINT FK_friends_sender 
+FOREIGN KEY (sent_by_id) REFERENCES users(id);
