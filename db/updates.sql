56efcf2e78bb65b4a8bc50c5e03505eff73218d0
Marev-Wasim <marevwasim79@gmail.com>
2025-12-23 21:03:46 +0200

    Update updates.sql

--- db/updates.sql
+++ db/updates.sql
@@ -4,9 +4,16 @@ GO
 ALTER TABLE study_logs 
 ADD task_id INT NULL;
 
--- ALTER TABLE study_logs 
--- ADD CONSTRAINT study_logs_task 
--- FOREIGN KEY (task_id) REFERENCES tasks(task_id);
+ALTER TABLE study_logs 
+ALTER COLUMN subject_id INT NULL;
+
+ALTER TABLE study_logs
+ALTER COLUMN hours_studied DECIMAL(6, 2);
+
+ALTER TABLE study_logs
+ADD CONSTRAINT study_logs_task 
+FOREIGN KEY (task_id) REFERENCES tasks(task_id) 
+ON DELETE NO ACTION;
 
 ALTER TABLE friends 
 ADD sent_by_id INT NOT NULL;
@@ -14,8 +21,3 @@ ADD sent_by_id INT NOT NULL;
 ALTER TABLE friends 
 ADD CONSTRAINT FK_friends_sender 
 FOREIGN KEY (sent_by_id) REFERENCES users(id);
-
-ALTER TABLE study_logs
-ADD CONSTRAINT study_logs_task 
-FOREIGN KEY (task_id) REFERENCES tasks(task_id) 
-ON DELETE NO ACTION;
