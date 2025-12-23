9897778ca114fb0c69e46c7494145b714ab9d977
Marev-Wasim <marevwasim79@gmail.com>
2025-12-23 22:18:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,12 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # To handle database connection stability
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,  # Checks connection health before every query
+        "pool_recycle": 300,    # Re-opens connections every 5 minutes
+    }
     
     if not SECRET_KEY or not SQLALCHEMY_DATABASE_URI:
         raise ValueError("Missing required environment variables")
